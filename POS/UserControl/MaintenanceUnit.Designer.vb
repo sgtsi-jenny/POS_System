44d5a50592170446f99b81c19b77d64026f0da66
sgtsi-jenny <jenny.bercasio@sparkglobaltech.com>
2016-02-05 18:06:09 +0800

    nw usc Maintenance Unit

---
+++ POS/UserControl/MaintenanceUnit.Designer.vb
@@ -0,0 +1,271 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MaintenanceUnit
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pnlMain = New System.Windows.Forms.Panel()
+        Me.btnEdit = New System.Windows.Forms.Button()
+        Me.txtDescription = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtunitId = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.lvUnit = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.pnlMain.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlMain
+        '
+        Me.pnlMain.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pnlMain.Controls.Add(Me.btnCancel)
+        Me.pnlMain.Controls.Add(Me.btnEdit)
+        Me.pnlMain.Controls.Add(Me.txtDescription)
+        Me.pnlMain.Controls.Add(Me.Label2)
+        Me.pnlMain.Controls.Add(Me.txtunitId)
+        Me.pnlMain.Controls.Add(Me.Label3)
+        Me.pnlMain.Controls.Add(Me.btnDelete)
+        Me.pnlMain.Controls.Add(Me.btnSave)
+        Me.pnlMain.Controls.Add(Me.btnAdd)
+        Me.pnlMain.Controls.Add(Me.lvUnit)
+        Me.pnlMain.Controls.Add(Me.Label10)
+        Me.pnlMain.Controls.Add(Me.Label4)
+        Me.pnlMain.Location = New System.Drawing.Point(0, -1)
+        Me.pnlMain.Name = "pnlMain"
+        Me.pnlMain.Size = New System.Drawing.Size(837, 411)
+        Me.pnlMain.TabIndex = 7
+        '
+        'btnEdit
+        '
+        Me.btnEdit.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnEdit.BackColor = System.Drawing.Color.FromArgb(CType(CType(247, Byte), Integer), CType(CType(202, Byte), Integer), CType(CType(24, Byte), Integer))
+        Me.btnEdit.FlatAppearance.BorderSize = 0
+        Me.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnEdit.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEdit.ForeColor = System.Drawing.Color.Black
+        Me.btnEdit.Location = New System.Drawing.Point(395, 1)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(102, 54)
+        Me.btnEdit.TabIndex = 207
+        Me.btnEdit.Text = "Edit"
+        Me.btnEdit.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btnEdit.UseVisualStyleBackColor = False
+        '
+        'txtDescription
+        '
+        Me.txtDescription.BackColor = System.Drawing.Color.White
+        Me.txtDescription.Enabled = False
+        Me.txtDescription.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDescription.ForeColor = System.Drawing.Color.Black
+        Me.txtDescription.Location = New System.Drawing.Point(177, 125)
+        Me.txtDescription.Name = "txtDescription"
+        Me.txtDescription.Size = New System.Drawing.Size(252, 29)
+        Me.txtDescription.TabIndex = 204
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(73, 133)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(98, 21)
+        Me.Label2.TabIndex = 206
+        Me.Label2.Text = "Description"
+        '
+        'txtunitId
+        '
+        Me.txtunitId.BackColor = System.Drawing.Color.White
+        Me.txtunitId.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtunitId.ForeColor = System.Drawing.Color.Black
+        Me.txtunitId.Location = New System.Drawing.Point(177, 84)
+        Me.txtunitId.Name = "txtunitId"
+        Me.txtunitId.ReadOnly = True
+        Me.txtunitId.Size = New System.Drawing.Size(43, 29)
+        Me.txtunitId.TabIndex = 203
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Black
+        Me.Label3.Location = New System.Drawing.Point(73, 92)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(27, 21)
+        Me.Label3.TabIndex = 205
+        Me.Label3.Text = "ID"
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnDelete.BackColor = System.Drawing.Color.FromArgb(CType(CType(247, Byte), Integer), CType(CType(202, Byte), Integer), CType(CType(24, Byte), Integer))
+        Me.btnDelete.FlatAppearance.BorderSize = 0
+        Me.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnDelete.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.ForeColor = System.Drawing.Color.Black
+        Me.btnDelete.Location = New System.Drawing.Point(717, 1)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(102, 54)
+        Me.btnDelete.TabIndex = 202
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btnDelete.UseVisualStyleBackColor = False
+        '
+        'btnSave
+        '
+        Me.btnSave.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSave.BackColor = System.Drawing.Color.FromArgb(CType(CType(247, Byte), Integer), CType(CType(202, Byte), Integer), CType(CType(24, Byte), Integer))
+        Me.btnSave.FlatAppearance.BorderSize = 0
+        Me.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSave.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSave.ForeColor = System.Drawing.Color.Black
+        Me.btnSave.Location = New System.Drawing.Point(609, 0)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(102, 54)
+        Me.btnSave.TabIndex = 200
+        Me.btnSave.Text = "Save"
+        Me.btnSave.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btnSave.UseVisualStyleBackColor = False
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnAdd.BackColor = System.Drawing.Color.FromArgb(CType(CType(247, Byte), Integer), CType(CType(202, Byte), Integer), CType(CType(24, Byte), Integer))
+        Me.btnAdd.FlatAppearance.BorderSize = 0
+        Me.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnAdd.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.ForeColor = System.Drawing.Color.Black
+        Me.btnAdd.Location = New System.Drawing.Point(287, 0)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(102, 54)
+        Me.btnAdd.TabIndex = 110
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btnAdd.UseVisualStyleBackColor = False
+        '
+        'lvUnit
+        '
+        Me.lvUnit.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lvUnit.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2})
+        Me.lvUnit.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lvUnit.FullRowSelect = True
+        Me.lvUnit.GridLines = True
+        Me.lvUnit.Location = New System.Drawing.Point(70, 186)
+        Me.lvUnit.Name = "lvUnit"
+        Me.lvUnit.Size = New System.Drawing.Size(503, 192)
+        Me.lvUnit.TabIndex = 81
+        Me.lvUnit.UseCompatibleStateImageBehavior = False
+        Me.lvUnit.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Unit Id"
+        Me.ColumnHeader1.Width = 100
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Description"
+        Me.ColumnHeader2.Width = 601
+        '
+        'Label10
+        '
+        Me.Label10.BackColor = System.Drawing.Color.FromArgb(CType(CType(245, Byte), Integer), CType(CType(215, Byte), Integer), CType(CType(110, Byte), Integer))
+        Me.Label10.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label10.Font = New System.Drawing.Font("Segoe UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.ForeColor = System.Drawing.Color.Black
+        Me.Label10.Location = New System.Drawing.Point(18, 0)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(387, 54)
+        Me.Label10.TabIndex = 201
+        Me.Label10.Text = "Unit Maintenance"
+        Me.Label10.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label4.BackColor = System.Drawing.Color.FromArgb(CType(CType(245, Byte), Integer), CType(CType(215, Byte), Integer), CType(CType(110, Byte), Integer))
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI Light", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Gold
+        Me.Label4.Location = New System.Drawing.Point(-8, 1)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(846, 54)
+        Me.Label4.TabIndex = 199
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnCancel.BackColor = System.Drawing.Color.FromArgb(CType(CType(247, Byte), Integer), CType(CType(202, Byte), Integer), CType(CType(24, Byte), Integer))
+        Me.btnCancel.FlatAppearance.BorderSize = 0
+        Me.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCancel.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCancel.ForeColor = System.Drawing.Color.Black
+        Me.btnCancel.Location = New System.Drawing.Point(502, 0)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(102, 54)
+        Me.btnCancel.TabIndex = 208
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btnCancel.UseVisualStyleBackColor = False
+        '
+        'MaintenanceUnit
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.pnlMain)
+        Me.Name = "MaintenanceUnit"
+        Me.Size = New System.Drawing.Size(837, 425)
+        Me.pnlMain.ResumeLayout(False)
+        Me.pnlMain.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents pnlMain As System.Windows.Forms.Panel
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents lvUnit As System.Windows.Forms.ListView
+    Friend WithEvents txtDescription As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtunitId As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents btnEdit As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+
+End Class
---
+++ POS/UserControl/MaintenanceUnit.vb
@@ -0,0 +1,112 @@
+﻿Public Class MaintenanceUnit
+    Dim db As New DBHelper(My.Settings.DBconn)
+    Dim dr As SqlClient.SqlDataReader
+    Dim cmd As SqlClient.SqlCommand
+    Dim rec As Integer
+    Dim data As New Dictionary(Of String, Object)
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        txtunitId.Text = ""
+        txtDescription.Text = ""
+        txtDescription.Enabled = True
+        txtDescription.Focus()
+    End Sub
+    Private Sub loadListview()
+        Dim parameters As New Dictionary(Of String, Object)
+        lvUnit.Items.Clear()
+        Dim Item As ListViewItem
+        Try
+            dr = db.ExecuteReader("Select unit_id, description from Units where is_deleted=0", parameters)
+            If dr.HasRows Then
+                Do While dr.Read
+                    Item = Me.lvUnit.Items.Add(dr.Item("unit_id").ToString)
+                    With Item
+                        .SubItems.Add(dr.Item("description"))
+                    End With
+                Loop
+            Else
+                MsgBox("No record of units", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "No unit")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        Finally
+            db.Dispose()
+        End Try
+    End Sub
+    Private Sub MaintenanceUnit_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        loadListview()
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        If txtDescription.Text = "" Then
+            MessageBox.Show("Add unit description", "Fill up the Form", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1)
+            Exit Sub
+        End If
+        If txtunitId.Text = "" Then
+            Dim rec As Integer
+            Dim data As New Dictionary(Of String, Object)
+            Try
+                data.Add("unit_id", txtunitId.Text)
+                data.Add("description", txtDescription.Text)
+
+                rec = db.ExecuteNonQuery("INSERT INTO Units(description) " & _
+                                         "VALUES(@description)", data)
+
+                If Not rec < 1 Then
+                    MessageBox.Show("Record saved!", "Important Message", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1)
+                    data.Clear()
+                    loadListview()
+                    txtDescription.Text = ""
+                    txtDescription.Enabled = False
+                End If
+            Catch ex As Exception
+                MsgBox(ex.ToString)
+            Finally
+                db.Dispose()
+            End Try
+
+        Else
+            Dim rec As Integer
+            Dim data As New Dictionary(Of String, Object)
+            Try
+                data.Add("unit_id", txtunitId.Text)
+                data.Add("description", txtDescription.Text)
+                rec = db.ExecuteNonQuery("UPDATE Units set description=@description WHERE unit_id = " & txtunitId.Text, data)
+
+                If Not rec < 1 Then
+                    MessageBox.Show("Record updated!", "Important Message", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1)
+                    data.Clear()
+                    loadListview()
+                    txtDescription.Text = ""
+                    txtunitId.Text = ""
+                    txtDescription.Enabled = False
+                End If
+            Catch ex As Exception
+                MsgBox(ex.ToString)
+            Finally
+                db.Dispose()
+            End Try
+        End If
+    End Sub
+
+    Private Sub lvUnit_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lvUnit.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub btnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
+        If lvUnit.SelectedItems.Count > 0 Then 'make sure there is a selected item to modify
+            txtDescription.Enabled = True
+            txtunitId.Text = lvUnit.FocusedItem.Text
+            txtunitId.Text = lvUnit.SelectedItems(0).SubItems(0).Text.ToString
+            txtDescription.Text = lvUnit.SelectedItems(0).SubItems(1).Text.ToString
+        Else
+            MessageBox.Show("Please select record to update.", "Important Note", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1)
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        txtDescription.Text = ""
+        txtunitId.Text = ""
+        txtDescription.Enabled = False
+    End Sub
+End Class
